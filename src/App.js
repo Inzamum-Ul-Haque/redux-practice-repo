f4838af741c224637b28af6b16f4b5e214f5b8b1
Inzamum-Ul-Haque <inzamum.inu@gmail.com>
2023-02-27 21:05:07 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useReducer } from "react";
+import "./App.css";
+import LongForm from "./components/LongForm";
 
 function App() {
+  //   const initialState = 0;
+  //   const reducer = (state, action) => {
+  //     if (action.type === "INCREMENT") {
+  //       return state + action.payload;
+  //     } else if (action.type === "DECREMENT") {
+  //       return state - action.payload;
+  //     } else {
+  //       return state;
+  //     }
+  //   };
+
+  //   const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <button onClick={() => dispatch({ type: "INCREMENT", payload: 5 })}>
+        Increase
+      </button>
+      <p>{state}</p>
+      <button onClick={() => dispatch({ type: "DECREMENT", payload: 3 })}>
+        Decrease
+      </button> */}
+      <LongForm />
     </div>
   );
 }
